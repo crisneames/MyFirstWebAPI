8beac506b122eb1282da6b22c8201aae100fdaee
crisneames <crisneames@gmail.com>
2023-02-18 07:23:56 -0600

    Add project files.

---
+++ Controllers/DistrictController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyFirstWebAPI.Models;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DistrictController : ControllerBase
+    {
+        [HttpGet]
+        public District Get()
+        {
+            return new District() { Id = 1, Name = "Middle TN" };
+        }
+
+        [HttpPost]
+        public IActionResult Post(District district)
+        {
+            return NotFound();
+        }
+    }
+}
